7a5bede29687be9c20b6047be27bd72ac19f1100
Lubeth <lubethjimenez@yahoo.com>
2026-03-10 19:53:23 -0400

    Add example of .then and how it is used

--- index.js
+++ index.js
@@ -1,3 +1,37 @@
 "use strict";
 
 console.log("Greetings!");
+
+//This is a .then Function Example and how it is used//
+async function getData(url, options){
+      try {
+        const res = await fetch(url, options)
+
+        if(!res.ok){
+            throw new Error("Failed to fetch")
+        }
+
+        const data = await res.json()
+        return data
+
+      } catch (error) {
+        console.log(error)
+      }
+} 
+const url = "https://pokeapi.co/api/v2/pokemon/"
+const options = {
+    method: "GET",
+};
+fetch(url, options)
+  .then((res) => {
+    if (!res.ok){
+        throw new Error("Failed to fetch")
+    }
+    res.json().then((data) => {
+        console.log(data);
+    });
+  })
+.catch((error) => console.error(error));
+//Then End of .then Example//
+
+
