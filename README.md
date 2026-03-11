cb9a409a287675fc4d6cf3a061402d9ceb42ec1f
Lubeth <lubethjimenez@yahoo.com>
2026-03-11 10:16:47 -0400

    Add Hashtag Function JS Example

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # week6-Classwork
+
+Headers vs Params
+
+-Prefer APIs with the key in the header
+
+ OAuth
+ -Login technique - Permissions are tied to a user
+  -comes with fine grained security
+JWT
+-JSON Web Token 
--- index.js
+++ index.js
@@ -22,16 +22,59 @@ const url = "https://pokeapi.co/api/v2/pokemon/"
 const options = {
     method: "GET",
 };
+
 fetch(url, options)
   .then((res) => {
+
     if (!res.ok){
         throw new Error("Failed to fetch")
     }
+
     res.json().then((data) => {
         console.log(data);
     });
   })
 .catch((error) => console.error(error));
 //Then End of .then Example//
+
+
+
+//JS Hashtag Function Asynchronous Example & What it does//
+async function hashStringSHA256(message) {
+  // Encode the string as a Uint8Array
+  const msgBuffer = new TextEncoder().encode(message);
+  // Hash the message using SHA-256
+  const hashBuffer = await crypto.subtle.digest("SHA-256", msgBuffer);
+  // Convert the ArrayBuffer to a hex string
+  const hashArray = Array.from(new Uint8Array(hashBuffer));
+  const hashHex = hashArray
+    .map((b) => ("00" + b.toString(16)).slice(-2))
+    .join("");
+  return hashHex;
+}
+
+// Example usage:
+const data = {
+  sub: "zNlGSgmCXqogKqCq4MWB",
+  email: "potato@example.com",
+  permisson: ["admin"],
+  iat: 1773191259,
+  exp: 1773796059,
+};
+const stringed = JSON.stringify(data + "ThisIsTheCodeSecret")
+
+async function hashData() {
+    try {
+        const hash = await hashStringSHA256(stringed);
+        console.log(hash)
+    } catch (error) {
+        
+    }
+}
+hashData();
+//Once something is hashed it is forever/indefinite//
+//Hashing is very important it's how you protect JWT Tokens//
+//End of Hashtag Function Example//
+
 
 
